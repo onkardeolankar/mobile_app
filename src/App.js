b1d51e821b9b89d1cc503595b3d0a300a304bc12
onkardeolankar1996@gmail.com <codewithkeeda@gmail.com>
2022-07-23 01:30:48 +0530

    deployed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import {useEffect, useState} from "react";
 
 function App() {
+
+	// const mobiles = [
+	// 	{
+	// 		model: "OnePlus 9 5G",
+	// 		img: "https://m.media-amazon.com/images/I/61fy+u9uqPL._SX679_.jpg",
+	// 		company: "Oneplus",
+	// 	},
+	// 	{
+	// 		model: "Iphone 13 mini",
+	// 		img: "https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/iphone-13-mini-blue-select-2021?wid=470&hei=556&fmt=jpeg&qlt=95&.v=1645572315986",
+	// 		company: "Apple",
+	// 	},
+	// 	{
+	// 		model: "Samsung s21 ultra",
+	// 		img: "https://m.media-amazon.com/images/I/81kfA-GtWwL._SY606_.jpg",
+	// 		company: "Samsung",
+	// 	},
+	// 	{
+	// 		model: "Xiomi mi 11",
+	// 		img: "https://m.media-amazon.com/images/I/51K4vNxMAhS._AC_SX522_.jpg",
+	// 		company: "Xiomi",
+	// 	},
+	// ];
+  const [mobiles, setMobiles] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:4000/mobiles")
+    .then((data) => data.json())
+    .then((mbs) => setMobiles(mbs));
+  },[]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+		<div className="App">
+			<div className="phone-list-container">
+				{mobiles.map((mb) => (
+					<Phone key={mb._id} mobile={mb} />
+				))}
+			</div>
+		</div>
+	);
+}
+
+function Phone({ mobile }) {
+	return (
+		<div className="phone-container">
+			<img className="phone-picture" src={mobile.img} alt={mobile.model} />
+			<h2 className="phone-name">{mobile.model}</h2>
+			<p className="phone-company">{mobile.company}</p>
+		</div>
+	);
 }
 
 export default App;
